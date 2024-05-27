1115d1ffd75a5560318e7ee4d86e28783e52d42c
LOPAMUDRA MUKHERJEE <139487305+LOPAMUDRA-MUKHERJEE@users.noreply.github.com>
2024-05-27 15:56:36 +0530

    Add files via upload
    Navbar under construction

---
+++ chromatic_canvas/script.js
@@ -0,0 +1,10 @@
+// {/* <script>
+// function toggleNav() {
+//     document.querySelector('.nav-toggle-btn').classList.toggle('active');
+// }
+// </script> */}
+// JavaScript function to toggle the 'active' class on the button
+function toggleNav() {
+    document.querySelector('.nav-toggle-btn').classList.toggle('active');
+}
+
